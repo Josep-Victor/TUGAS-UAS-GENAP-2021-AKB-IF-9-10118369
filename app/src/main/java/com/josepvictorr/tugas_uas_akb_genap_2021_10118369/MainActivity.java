856f22ff6834ab0fe9dc540df5b9744263769b01
Josep Victor <josepvictorrrrr@gmail.com>
2021-08-08 15:38:37 +0700

    First Commit icon & splash screen

---
+++ app/src/main/java/com/josepvictorr/tugas_uas_akb_genap_2021_10118369/MainActivity.java
@@ -0,0 +1,14 @@
+package com.josepvictorr.tugas_uas_akb_genap_2021_10118369;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/josepvictorr/tugas_uas_akb_genap_2021_10118369/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.josepvictorr.tugas_uas_akb_genap_2021_10118369;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 3000L);
+    }
+}
